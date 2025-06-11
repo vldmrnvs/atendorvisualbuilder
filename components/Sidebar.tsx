1e74d5f81290d73e1050939c916b56e97ad7b084
vldmrnvs <vldmrnvs@gmail.com>
2025-06-12 00:55:10 +0200

    chore: remove unused router import

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,6 @@
 'use client'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
 import logo from '../assets/atendor-logo.svg'
 
 export default function Sidebar() {
